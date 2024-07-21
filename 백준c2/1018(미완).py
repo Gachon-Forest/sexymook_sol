b927d1f6962bd66273e43e7cfd95b39cba2d9fed
sexymook <jungmooklee@naver.com>
2024-07-21 23:53:17 +0900

    sexy

--- 백준c2/1018(미완).py
+++ 백준c2/1018(미완).py
@@ -3,10 +3,38 @@ import sys
 input = sys.stdin.readline
 x,y = map(int,input().split())
 cheseBord = []
-for i in range(y):
-    a= input()
-    a = a.replace('\n','')
-    cheseBord.append(a)
+result = []
+for _ in range(x):
+    cheseBord.append(input())
+for i in range(x-7):
+    for j in range(y-7):
+        numx = 0
+        numy = 0
+        for a in range(i,i+8):
+            for b in range(j,j+8):
+                if (a+b)%2 == 0:
+                    if cheseBord[a][b] != 'W':
+                        numx +=1
+                    else:
+                        numy +=1
+                else:
+                    if cheseBord[a][b] != 'B':
+                        numx += 1
+                    else:
+                        numy += 1
+
+    result.append(numx)
+    result.append(numy)
+print(min(result))
+
+
+
+
+
+
+
+
+
 
 
 
